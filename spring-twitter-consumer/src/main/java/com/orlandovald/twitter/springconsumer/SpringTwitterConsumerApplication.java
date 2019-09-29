8a086d3d2c114d7e83b1ff489dd75e088bd1b428
Orlando Valdez <orlandovald@gmail.com>
2019-09-28 21:24:38 -0400

    Add Spring Consumer module skeleton

---
+++ spring-twitter-consumer/src/main/java/com/orlandovald/twitter/springconsumer/SpringTwitterConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.orlandovald.twitter.springconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTwitterConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTwitterConsumerApplication.class, args);
+	}
+
+}
